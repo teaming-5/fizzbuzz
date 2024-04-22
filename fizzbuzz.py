5154086edf35d76ca143d31bba37b850da268546
Lucy <lucypothesis@gmail.com>
2024-04-22 18:08:10 +0900

    fizzbuzz를 구현했습니다.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,5 @@
-print('main.py')
+for i in range(1,16):
+    if i % 3 == 0 or i % 5 == 0:
+        print('fizz' * (1 if i % 3 == 0 else 0) + 'buzz' * (1 if i % 5 == 0 else 0)
+    else:
+        print(i)
